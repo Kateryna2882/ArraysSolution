9ab3ef258b86d364f070d4d9cce2da91befe5138
Kateryna2882 <govoryhina82@gmail.com>
2025-01-06 20:20:40 +0200

    add solution

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] arrays = new String[10];
+        for (int i = 0; i < 8; i++) {
+            arrays[i] = reader.readLine();
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(arrays[i]);
         }
     }
 }
