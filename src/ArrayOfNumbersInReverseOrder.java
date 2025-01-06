38f5b8834f30dc69667820b59388957527638266
Kateryna2882 <govoryhina82@gmail.com>
2025-01-06 21:00:58 +0200

    add solution

--- src/ArrayOfNumbersInReverseOrder.java
+++ src/ArrayOfNumbersInReverseOrder.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class ArrayOfNumbersInReverseOrder {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter 10 numbers in reverse order: ");
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(array[i]);
+        }
+    }
 }
