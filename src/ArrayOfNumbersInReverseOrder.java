969526f1e552a8e3d5879e4c4e70bffa44771cf9
Kateryna2882 <govoryhina82@gmail.com>
2025-01-07 18:25:40 +0200

    add solution

--- src/ArrayOfNumbersInReverseOrder.java
+++ src/ArrayOfNumbersInReverseOrder.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 
 public class ArrayOfNumbersInReverseOrder {
     public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter 10 numbers in reverse order: ");
         int[] array = new int[10];
         for (int i = 0; i < 10; i++) {
--- src/OneLargeArrayAndTwoSmallOnes.java
+++ src/OneLargeArrayAndTwoSmallOnes.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class OneLargeArrayAndTwoSmallOnes {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] array = new int[20];
+        for (int i = 0; i < 20; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array1[i] = array[i];
+            array2[i] = array[i + 10];
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(array2[i]);
+        }
+    }
 }
+
