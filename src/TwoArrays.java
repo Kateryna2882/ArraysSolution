c38765d9508b0fcc23ca1298b205226673ebcc40
Kateryna2882 <govoryhina82@gmail.com>
2025-01-09 18:48:19 +0200

    add solution

--- src/TwoArrays.java
+++ src/TwoArrays.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class TwoArrays {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = new String[10];
+        int[] arrays = new int[10];
+        for (int i = 0; i < 10; i++) {
+            strings[i] = reader.readLine();
+            arrays[i] = strings[i].length();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arrays[i]);
+        }
+    }
 }
