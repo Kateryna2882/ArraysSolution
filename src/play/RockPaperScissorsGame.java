a8e70534b9ed1f869934ab58ffec8d1ac508f332
Kateryna2882 <govoryhina82@gmail.com>
2025-01-13 18:44:52 +0200

    add Player class

--- src/play/RockPaperScissorsGame.java
+++ src/play/RockPaperScissorsGame.java
@@ -1,4 +1,11 @@
 package play;
 
 public class RockPaperScissorsGame {
+    private Player player;
+    private Player computer;
+    private ScoreBoard board;
+    {
+        computer = new Player();
+        computer.
+    }
 }
