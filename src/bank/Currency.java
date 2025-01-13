f74266d069637fd4918a8aad48cd96afc374fe9a
Kateryna2882 <govoryhina82@gmail.com>
2025-01-13 18:05:22 +0200

    add solution

--- src/bank/Currency.java
+++ src/bank/Currency.java
@@ -1,4 +1,31 @@
 package bank;
 
-public enum Currency {
+
+enum Currency {
+    USD(1.0),
+    EUR(1.1),
+    GBP(1.3),
+    JPY(0.009);
+    private final double exchangeRateToUSD;
+
+    Currency(double exchangeRateToUSD) {
+        this.exchangeRateToUSD = exchangeRateToUSD;
+    }
+
+    public double getExchangeRateToUSD() {
+        return exchangeRateToUSD;
+    }
+
+    public static void main(String[] args) {
+
+        for (Currency currency : Currency.values()) {
+            if (currency != Currency.USD) {
+                System.out.printf("Converting %s to USD=%f%n", currency.name(),
+                        currency.getExchangeRateToUSD());
+            }
+        }
+
+    }
+
 }
+
