e1db5029f73286b256d8c83bb5801edf7f7ad7c2
Kateryna2882 <govoryhina82@gmail.com>
2025-01-17 20:28:00 +0200

    add solution and add CreatingTwoDimensionalArray class

---
+++ src/twoDimensionalArrays/CreatingTwoDimensionalArray.java
@@ -0,0 +1,4 @@
+package twoDimensionalArrays;
+
+public class CreatingTwoDimensionalArray {
+}
--- src/twoDimensionalArrays/TriangularArray.java
+++ src/twoDimensionalArrays/TriangularArray.java
@@ -7,28 +7,34 @@ public class TriangularArray {
     public static int[][] result = new int[10][];
 
     public static void main(String[] args) {
-        //write your code here
         // Ініціалізуємо трикутний масив
+//        for (int i = 0; i < result.length; i++) {
+//            result[i] = new int[i + 1];
+//        }
+//        // Заповнюємо масив сумами індексів
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result[i].length; j++) {
+//                result[i][j] = i + j;
+//            }
+//        }
+//        // Виводимо масив на екран
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result[i].length; j++) {
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+
         for (int i = 0; i < result.length; i++) {
             result[i] = new int[i + 1];
-        }
-
-        // Заповнюємо масив сумами індексів
-        for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result[i].length; j++) {
                 result[i][j] = i + j;
-            }
-        }
-
-        // Виводимо масив на екран
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
                 System.out.print(result[i][j] + " ");
             }
             System.out.println();
         }
     }
 }
+
 
 
 
