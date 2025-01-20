915d3b1792bd6b656b3ef34c36f54fffe33ecf13
Kateryna2882 <govoryhina82@gmail.com>
2025-01-20 16:38:12 +0200

    add classes and solution

--- src/twoDimensionalArrays/CreatingTwoDimensionalArray.java
+++ src/twoDimensionalArrays/CreatingTwoDimensionalArray.java
@@ -1,4 +1,21 @@
 package twoDimensionalArrays;
 
+import java.util.Scanner;
+
 public class CreatingTwoDimensionalArray {
+
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        multiArray = new int[n][];
+        for (int i = 0; i < n; i++) {
+            int size = Integer.parseInt(scanner.nextLine());
+            multiArray[i] = new int[size];
+        }
+    }
 }
