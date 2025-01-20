7a59ec0a3bc51ef12b14bc040438138d0845184d
Kateryna2882 <govoryhina82@gmail.com>
2025-01-20 20:13:07 +0200

    add solution

--- src/twoDimensionalArrays/DisplayingArrays.java
+++ src/twoDimensionalArrays/DisplayingArrays.java
@@ -1,4 +1,16 @@
 package twoDimensionalArrays;
 
+import java.util.Arrays;
+
 public class DisplayingArrays {
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        String Strstrings = Arrays.toString(strings);
+        String Strints = Arrays.toString(ints);
+        System.out.println(Strstrings);
+        System.out.println(Strints);
+
+    }
 }
