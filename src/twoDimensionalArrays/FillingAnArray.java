7d2237e7a96378f61f3720cd5c6deacbbef48a9d
Kateryna2882 <govoryhina82@gmail.com>
2025-01-22 17:10:45 +0200

    add solution

--- src/twoDimensionalArrays/FillingAnArray.java
+++ src/twoDimensionalArrays/FillingAnArray.java
@@ -1,4 +1,24 @@
 package twoDimensionalArrays;
 
+import java.util.Arrays;
+
+/*
+Filling an array
+*/
+
 public class FillingAnArray {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //write your code here
+
+        int firstPart = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
+        Arrays.fill(array, 0, firstPart, valueStart);
+        Arrays.fill(array, firstPart, array.length, valueEnd);
+        System.out.println(Arrays.toString(array));
+    }
+
 }
