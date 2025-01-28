22b1e7c7e011319c56a7ba684b3129f520a54b66
Kateryna2882 <govoryhina82@gmail.com>
2025-01-28 21:28:22 +0200

    add solution

--- src/methods/OrderlyInformation.java
+++ src/methods/OrderlyInformation.java
@@ -1,4 +1,19 @@
 package methods;
 
 public class OrderlyInformation {
+
+    public static void main(String[] args) {
+        //write your code here
+        print1();
+        print2();
+    }
+
+    public static void print1() {
+        System.out.println("For sweetest things turn sourest by their deeds;");
+    }
+
+    public static void print2() {
+        System.out.println("Lilies that fester smell far worse than weeds.");
+    }
+
 }
