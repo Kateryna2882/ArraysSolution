b679face014e9bcb103a89a41c7b83c25a28e15f
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 18:49:14 +0200

    add WelcomeButNotEveryone class

---
+++ src/methods/WelcomeButNotEveryone.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class WelcomeButNotEveryone {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void signIn(String username) {
+        //write your code here
+        System.out.println("Welcome, " + username);
+        System.out.println("We've really missed you, " + username);
+    }
+}
+
+
