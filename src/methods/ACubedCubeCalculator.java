c222968c0471ea61105f2f9c790a839401099025
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 19:26:32 +0200

    add ACubedCubeCalculator class

---
+++ src/methods/ACubedCubeCalculator.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class ACubedCubeCalculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a) {
+        return a * a * a;
+    }
+
+    //write your code here
+}
