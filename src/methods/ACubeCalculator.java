755152e7bfe1c434809843bbab17f36ff65dbae0
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 19:25:13 +0200

    add solution

--- src/methods/ACubeCalculator.java
+++ src/methods/ACubeCalculator.java
@@ -3,9 +3,11 @@ package methods;
 public class ACubeCalculator {
 
     public static void main(String[] args) {
-
     }
 
     //write your code here
+    public static long cube(long a) {
+        return a * a * a;
+    }
 
 }
