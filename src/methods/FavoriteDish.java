aaea15ed9fce3b663c0e4709793826df3dc9d02e
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 17:52:27 +0200

    add solution

--- src/methods/FavoriteDish.java
+++ src/methods/FavoriteDish.java
@@ -1,4 +1,22 @@
 package methods;
 
 public class FavoriteDish {
+
+
+    public static void main(String[] args) {
+        String firstName = "Olivia";
+        String lastName = "Klinger";
+        String favoriteDish = "Spaghetti";
+        printPersonInfo(firstName, lastName, favoriteDish);
+
+    }
+
+    public static void printPersonInfo(String name, String surname, String mel) {
+        System.out.println("Short dossier:");
+        System.out.println("First name: " + name);
+        System.out.println("Last name: " + surname);
+        System.out.println("Favorite dish: " + mel);
+
+
+    }
 }
