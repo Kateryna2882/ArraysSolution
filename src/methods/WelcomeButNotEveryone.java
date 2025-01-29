307d0a1f3ca21997525597f262b64bf702197579
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 19:16:01 +0200

    add solution

--- src/methods/WelcomeButNotEveryone.java
+++ src/methods/WelcomeButNotEveryone.java
@@ -3,11 +3,17 @@ package methods;
 public class WelcomeButNotEveryone {
 
     public static void main(String[] args) {
-
+        signIn("user");
+        signIn("username");
     }
 
     public static void signIn(String username) {
         //write your code here
+        String user = "user";
+        if (username.equals(user)) {
+            return;
+
+        }
         System.out.println("Welcome, " + username);
         System.out.println("We've really missed you, " + username);
     }
