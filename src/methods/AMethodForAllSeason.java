e82e2df8aa5309c1d7903d28f4bf83a98341bf12
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 18:32:54 +0200

    add AMethodForAllSeason class

---
+++ src/methods/AMethodForAllSeason.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class AMethodForAllSeason {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    //write your code here
+
+}
