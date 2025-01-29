7ce44eb806bf59c3bf7025703e85c6df6563c106
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 18:06:11 +0200

    add CorrectOrder class

---
+++ src/methods/CorrectOrder.java
@@ -0,0 +1,21 @@
+package methods;
+
+public class CorrectOrder {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        //write your code here
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
