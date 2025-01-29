899348aaff6eb974b96de9372dced22cdccef607
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 20:45:25 +0200

    add MinimumOfFourNumbers

---
+++ src/methods/MinimumOfFourNumbers.java
@@ -0,0 +1,23 @@
+package methods;
+
+public class MinimumOfFourNumbers {
+
+    public static int min(int a, int b, int c, int d) {
+        //write your code here
+
+    }
+
+    public static int min(int a, int b) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
+
+}
