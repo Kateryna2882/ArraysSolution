4a0857a4defa14429f7b800d00854e201c76e069
Kateryna2882 <govoryhina82@gmail.com>
2025-01-29 19:50:55 +0200

    add MinimumOfTwoNumbers class

---
+++ src/methods/MinimumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class MinimumOfTwoNumbers {
+
+    public static int min(int a, int b) {
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+        System.out.println(min(12, 33));
+        System.out.println(min(-20, 0));
+        System.out.println(min(-10, -20));
+    }
+
+}
