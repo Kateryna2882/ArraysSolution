d2ba421afe3673074273e4318424541f93a8226b
Kateryna2882 <govoryhina82@gmail.com>
2025-01-31 21:18:29 +0200

    add PrintThreeTimes class

---
+++ src/methods/PrintThreeTimes.java
@@ -0,0 +1,15 @@
+package methods;
+
+public class PrintThreeTimes {
+
+    public static void print3(String text) {
+        //write your code here
+
+    }
+
+    public static void main(String[] args) {
+        print3("window");
+        print3("file");
+    }
+
+}
