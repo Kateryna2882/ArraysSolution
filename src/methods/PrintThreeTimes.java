6cbd3b0049b3999897211a4f5b0126f82eef5506
Kateryna2882 <govoryhina82@gmail.com>
2025-01-31 21:25:47 +0200

    add solution

--- src/methods/PrintThreeTimes.java
+++ src/methods/PrintThreeTimes.java
@@ -4,6 +4,12 @@ public class PrintThreeTimes {
 
     public static void print3(String text) {
         //write your code here
+        System.out.print(text);
+        System.out.print(" ");
+        System.out.print(text);
+        System.out.print(" ");
+        System.out.print(text);
+        System.out.print(" ");
 
     }
 
