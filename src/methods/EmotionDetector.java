bb733340ed25b0c1d182c2d4581c3d874f9e0c25
Kateryna2882 <govoryhina82@gmail.com>
2025-02-03 13:05:21 +0200

    add EmotionDetector class

---
+++ src/methods/EmotionDetector.java
@@ -0,0 +1,11 @@
+package methods;
+
+import java.util.Scanner;
+
+public class EmotionDetector {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int emotionNumber = scanner.nextInt();
+
+    }
+}
