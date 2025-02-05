68374ae341290d8593fb9693c7a78eded21530f3
Kateryna2882 <govoryhina82@gmail.com>
2025-02-05 19:10:10 +0200

    add EmployeeRecords class

---
+++ src/variables/EmployeeRecords.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class EmployeeRecords {
+
+    public static final String NAME = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+}
