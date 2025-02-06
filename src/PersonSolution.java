db8c2b86086f666a4eb3e80e7054d3d16a21d26e
Kateryna2882 <govoryhina82@gmail.com>
2025-02-06 19:05:42 +0200

    add fields

--- src/PersonSolution.java
+++ src/PersonSolution.java
@@ -2,9 +2,15 @@ public class PersonSolution {
 
     public static void main(String[] args) {
         //write your code here
+        Person person = new Person();
+
     }
 
     public static class Person {
         //write your code here
+        String name;
+        int age;
+        int weight;
+        int money;
     }
 }
