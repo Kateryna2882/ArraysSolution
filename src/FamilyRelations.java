a1e9b54420a62e837600c0a830de47b0e2d610ec
Kateryna2882 <govoryhina82@gmail.com>
2025-02-07 21:01:19 +0200

    add FamilyRelations class

---
+++ src/FamilyRelations.java
@@ -0,0 +1,19 @@
+public class FamilyRelations {
+    public static void main(String[] args) {
+        //write your code here
+
+    }
+
+    public static class Man {
+        public int age;
+        public int height;
+        public Woman wife;
+    }
+
+    public static class Woman {
+        public int age;
+        public int height;
+        public Man husband;
+    }
+
+}
