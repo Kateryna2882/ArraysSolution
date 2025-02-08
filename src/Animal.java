7ed79d21df58a34214fbcc89fd6ceac21aa83f4e
Kateryna2882 <govoryhina82@gmail.com>
2025-02-08 20:16:53 +0200

    add Animal class and fields

---
+++ src/Animal.java
@@ -0,0 +1,29 @@
+public class Animal {
+
+    public static void main(String[] args) {
+        //write your code here
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Woman woman = new Woman();
+        cat.owner = woman;
+        dog.owner = woman;
+        fish.owner = woman;
+    }
+
+    public static class Cat {
+        public Woman owner;
+    }
+
+    public static class Dog {
+        public Woman owner;
+    }
+
+    public static class Fish {
+        public Woman owner;
+    }
+
+    public static class Woman {
+    }
+
+}
