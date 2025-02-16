5a72030ed525b1508c4daf4ee07f5f41773bc58f
Kateryna2882 <govoryhina82@gmail.com>
2025-02-16 19:52:34 +0200

    add fields

--- src/BinaryConverter.java
+++ src/BinaryConverter.java
@@ -8,11 +8,29 @@ public class BinaryConverter {
 
     public static String toBinary(int decimalNumber) {
         //write your code here
-        return null;
+        String binaryNumber = " ";
+        if (decimalNumber <= 0) {
+            return binaryNumber;
+        }
+        while (decimalNumber != 0) {
+            binaryNumber = decimalNumber % 2 + binaryNumber;
+            decimalNumber = decimalNumber / 2;
+        }
+        return binaryNumber;
     }
 
     public static int toDecimal(String binaryNumber) {
         //write your code here
-        return 0;
+        int decimalNumber =0;
+        if(binaryNumber==null){
+            return decimalNumber;
+        }
+        for (int i = 0; i < binaryNumber.length(); i++) {
+
+            int index = binaryNumber.length() - 1 - i;
+            int value = Character.getNumericValue(binaryNumber.charAt(index));
+            decimalNumber += value * Math.pow(2, i);
+        }
+        return decimalNumber;
     }
 }
