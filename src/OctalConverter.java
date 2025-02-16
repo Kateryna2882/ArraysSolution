f3457977891c1791e09f11a8269ed2890693c805
Kateryna2882 <govoryhina82@gmail.com>
2025-02-16 19:19:10 +0200

    add OctalConverter class

---
+++ src/OctalConverter.java
@@ -0,0 +1,19 @@
+public class OctalConverter {
+
+    public static void main(String[] args) {
+        int decimalNumber = 21;
+        System.out.println("Decimal number " + decimalNumber + " is equal to octal number " + toOctal(decimalNumber));
+        int octalNumber = 25;
+        System.out.println("Octal number " + octalNumber + " is equal to decimal number " + toDecimal(octalNumber));
+    }
+
+    public static int toOctal(int decimalNumber) {
+        //write your code here
+        return 0;
+    }
+
+    public static int toDecimal(int octalNumber) {
+        //write your code here
+        return 0;
+    }
+}
