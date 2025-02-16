d0de383a04d04882145df2d8008ffa80ae13c457
Kateryna2882 <govoryhina82@gmail.com>
2025-02-16 19:33:59 +0200

    add BinaryConverter class

---
+++ src/BinaryConverter.java
@@ -0,0 +1,18 @@
+public class BinaryConverter {
+    public static void main(String[] args) {
+        int decimalNumber = Integer.MAX_VALUE;
+        System.out.println("Decimal number " + decimalNumber + " is equal to binary number " + toBinary(decimalNumber));
+        String binaryNumber = "1111111111111111111111111111111";
+        System.out.println("Binary number " + binaryNumber + " is equal to decimal number " + toDecimal(binaryNumber));
+    }
+
+    public static String toBinary(int decimalNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static int toDecimal(String binaryNumber) {
+        //write your code here
+        return 0;
+    }
+}
