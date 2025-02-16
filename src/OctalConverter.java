af95726040682fd38170fab28bb32a5bd984788d
Kateryna2882 <govoryhina82@gmail.com>
2025-02-16 19:25:11 +0200

    add fields

--- src/OctalConverter.java
+++ src/OctalConverter.java
@@ -8,12 +8,30 @@ public class OctalConverter {
     }
 
     public static int toOctal(int decimalNumber) {
-        //write your code here
-        return 0;
+        int octalNumber = 0;
+        if (decimalNumber <= 0) {
+            return octalNumber;
+        }
+
+        int i = 0;
+        while (decimalNumber != 0) {
+            octalNumber = (int) (octalNumber + (decimalNumber % 8) * Math.pow(10, i++));
+            decimalNumber = decimalNumber / 8;
+        }
+        return octalNumber;
     }
 
     public static int toDecimal(int octalNumber) {
-        //write your code here
-        return 0;
+        int decimalNumber = 0;
+        if (octalNumber <= 0) {
+            return decimalNumber;
+        }
+
+        int i = 0;
+        while (octalNumber != 0) {
+            decimalNumber = (int) (decimalNumber + (octalNumber % 10) * Math.pow(8, i++));
+            octalNumber = octalNumber / 10;
+        }
+        return decimalNumber;
     }
 }
