22f59a25ade14c89cbe81794574d9e542f0508e8
Kateryna2882 <govoryhina82@gmail.com>
2025-02-19 18:24:15 +0200

    add HexadecimalConverter class

---
+++ src/HexadecimalConverter.java
@@ -0,0 +1,20 @@
+public class HexadecimalConverter {
+
+
+    public static void main(String[] args) {
+        int decimalNumber = 1256;
+        System.out.println("Decimal number " + decimalNumber + " is equal to hexadecimal number " + toHex(decimalNumber));
+        String hexNumber = "4e8";
+        System.out.println("Hexadecimal number " + hexNumber + " is equal to decimal number " + toDecimal(hexNumber));
+    }
+
+    public static String toHex(int decimalNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static int toDecimal(String hexNumber) {
+        //write your code here
+        return 0;
+    }
+}
