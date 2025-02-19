7566ca5b69c5af0c252d586c4007f871a4646ffc
Kateryna2882 <govoryhina82@gmail.com>
2025-02-19 18:31:37 +0200

    add BinaryToHexadecimalConverter class

---
+++ src/BinaryToHexadecimalConverter.java
@@ -0,0 +1,20 @@
+public class BinaryToHexadecimalConverter {
+
+    public static void main(String[] args) {
+        String binaryNumber = "100111010000";
+        System.out.println("Binary number " + binaryNumber + " is equal to hexadecimal number " + toHex(binaryNumber));
+        String hexNumber = "9d0";
+        System.out.println("Hexadecimal number " + hexNumber + " is equal to binary number " + toBinary(hexNumber));
+    }
+
+    public static String toHex(String binaryNumber) {
+        //write your code here
+        return null;
+    }
+
+    public static String toBinary(String hexNumber) {
+        //write your code here
+        return null;
+    }
+
+}
