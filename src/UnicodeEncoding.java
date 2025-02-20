fc188c331bc9a24f706e03068800c744f0c8f3e9
Kateryna2882 <govoryhina82@gmail.com>
2025-02-20 18:04:33 +0200

    add UnicodeEncoding class

---
+++ src/UnicodeEncoding.java
@@ -0,0 +1,11 @@
+public class UnicodeEncoding {
+    public static void main(String[] args) {
+        char[] symbols = new char[9];
+        init(symbols);
+        System.out.println(new String(symbols));
+    }
+
+    public static void init(char[] symbols) {
+        //write your code here
+    }
+}
