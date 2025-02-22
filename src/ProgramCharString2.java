551c68c31d0d95cbdd0115b471e7e871cee9ef63
Kateryna2882 <govoryhina82@gmail.com>
2025-02-22 18:54:37 +0200

    add fields

--- src/ProgramCharString2.java
+++ src/ProgramCharString2.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class ProgramCharString2 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Enter path: ");
+        System.out.println("Enter path: "); // "home/user/documents/file.txt"
         String path = console.nextLine();
-        String array[] = path.split("\\/");
-        String result= String.join("\\", array);
+
+        String result = path.replace("/", "\\"); // Заміна всіх '/' на '\'
+
         System.out.println(result);
+        console.close();
     }
 }
