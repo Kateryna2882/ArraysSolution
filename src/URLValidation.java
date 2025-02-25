f2e0a2a52804e275607fc6043e4d3591fbff8eea
Kateryna2882 <govoryhina82@gmail.com>
2025-02-25 21:20:46 +0200

    add fields

--- src/URLValidation.java
+++ src/URLValidation.java
@@ -1,9 +1,9 @@
 public class URLValidation {
-
     public static void main(String[] args) {
         String[] urls = {"https://javarush.us", "https://google.com",
                 "http://wikipedia.org", "facebook.com",
                 "https://instagram", "codegym.cc"};
+
         for (String url : urls) {
             String protocol = checkProtocol(url);
             String domainExtension = checkDomainExtension(url);
@@ -13,12 +13,24 @@ public class URLValidation {
     }
 
     public static String checkProtocol(String url) {
-        //write your code here
+        if (url.startsWith("http://")) {
+            return "http";
+        } else if (url.startsWith("https://")) {
+            return "https";
+        }
         return "unknown";
     }
 
     public static String checkDomainExtension(String url) {
-        //write your code here
+        if (url.endsWith(".com")) {
+            return "com";
+        } else if (url.endsWith(".net")) {
+            return "net";
+        } else if (url.endsWith(".org")) {
+            return "org";
+        } else if (url.endsWith(".us")) {
+            return "us";
+        }
         return "unknown";
     }
 }
