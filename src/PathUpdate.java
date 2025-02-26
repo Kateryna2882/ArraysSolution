9930101afd1323a6149b09f8d67719cf5dab363c
Kateryna2882 <govoryhina82@gmail.com>
2025-02-26 17:20:12 +0200

    add PathUpdate class

---
+++ src/PathUpdate.java
@@ -0,0 +1,14 @@
+public class PathUpdate {
+
+    public static void main(String[] args) {
+        String path = "/usr/java/jdk1.8/bin";
+
+        String jdk13 = "jdk-13";
+        System.out.println(changePath(path, jdk13));
+    }
+
+    public static String changePath(String path, String jdk) {
+        //write your code here
+        return null;
+    }
+}
