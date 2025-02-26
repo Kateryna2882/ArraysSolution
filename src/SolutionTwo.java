05ee9ccc32c5dd27385575666fcf42ac48cc64e6
Kateryna2882 <govoryhina82@gmail.com>
2025-02-26 18:08:52 +0200

    add SolutionTwo class and fields

---
+++ src/SolutionTwo.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SolutionTwo {
+    public static void main(String[] args) {
+        String packagePath = "java.util.stream";
+        String[] tokens = getTokens(packagePath, "\\.");
+        System.out.println(Arrays.toString(tokens));
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer stringTokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[stringTokenizer.countTokens()];
+        int i = 0;
+        while (stringTokenizer.hasMoreTokens()) {
+            result[i++] = stringTokenizer.nextToken();
+        }
+        return result;
+    }
+}
