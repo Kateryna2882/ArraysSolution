82909e5ad4944af10c30bebb64480b354d0fd86e
Kateryna2882 <govoryhina82@gmail.com>
2025-02-26 18:37:09 +0200

    add SolutionStringFormat class

---
+++ src/SolutionStringFormat.java
@@ -0,0 +1,12 @@
+public class SolutionStringFormat {
+
+    public static void main(String[] args) {
+        System.out.println(format("Amigo", 5000));
+    }
+
+    public static String format(String name, int salary) {
+        String phrase = "My name is . I will earn $ a month.";
+        //write your code here
+        return null;
+    }
+}
