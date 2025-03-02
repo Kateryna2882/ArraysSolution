d1d2bfe0915ef5c064090b1031c74c813e53641c
Kateryna2882 <govoryhina82@gmail.com>
2025-03-02 18:08:27 +0200

    add FlippingStrings class

---
+++ src/FlippingStrings.java
@@ -0,0 +1,14 @@
+public class FlippingStrings {
+
+    public static void main(String[] args) {
+        String string = "The cat is walking backwards";
+        System.out.println(string);
+        System.out.println(reverseString(string));
+    }
+
+    public static String reverseString(String string) {
+        //write your code here
+        return null;
+    }
+
+}
