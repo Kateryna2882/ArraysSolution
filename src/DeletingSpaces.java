cd0bdf15ea6c7770279a57aeda0198b9a6ede72e
Kateryna2882 <govoryhina82@gmail.com>
2025-03-03 13:36:51 +0200

    add DeletingSpaces class

---
+++ src/DeletingSpaces.java
@@ -0,0 +1,13 @@
+public class DeletingSpaces {
+
+    public static void main(String[] args) {
+        String stringWithoutSpaces = deleteSpaces("Good afternoon! How's life?");
+        System.out.println(stringWithoutSpaces);
+    }
+
+    public static String deleteSpaces(String string) {
+        //write your code here
+        return null;
+    }
+
+}
