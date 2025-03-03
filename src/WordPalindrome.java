c9146af51aa70089a22bb8067ed3b68d3fa0e32d
Kateryna2882 <govoryhina82@gmail.com>
2025-03-03 13:52:51 +0200

    add WordPalindrome class

---
+++ src/WordPalindrome.java
@@ -0,0 +1,14 @@
+public class WordPalindrome {
+
+    public static void main(String[] args) {
+        String word = "Rotor";
+        String answer = isPalindrome(word) ? "Yes" : "No";
+        System.out.println("Is the word \"" + word + "\" a palindrome? " + answer);
+    }
+
+    public static boolean isPalindrome(String word) {
+        //write your code here
+        return false;
+    }
+
+}
