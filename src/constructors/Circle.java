97e03ecbbf3649fef9950babc46289f67df86916
Kateryna2882 <govoryhina82@gmail.com>
2025-03-12 19:54:59 +0200

    add Circle class

---
+++ src/constructors/Circle.java
@@ -0,0 +1,12 @@
+package constructors;
+
+public class Circle {
+    public double x;
+    public double y;
+    public double r;
+
+    //write your code here
+    public static void main(String[] args) {
+
+    }
+}
