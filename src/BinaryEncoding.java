ab3cf7c131216d91b8cd34b161c44e28f3152540
Kateryna2882 <govoryhina82@gmail.com>
2025-03-16 16:23:27 +0200

    add BinaryEncoding class

---
+++ src/BinaryEncoding.java
@@ -0,0 +1,22 @@
+public class BinaryEncoding {
+
+
+    public static void main(String[] args) {
+        String string = "CodeGym!";
+        char[] charArray = string.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            print(charArray[i]);
+        }
+    }
+
+    private static void print(int number) {
+        String result = String.format("In Unicode, the character %s is %d, but in binary it is %s", (char) number, number, toBinary(number));
+        System.out.println(result);
+    }
+
+    public static String toBinary(int number) {
+        //write your code here
+        return null;
+    }
+
+}
