ac10e76e18208d9c9fc8b5076039d821a5f98051
Kateryna2882 <govoryhina82@gmail.com>
2025-03-18 20:37:29 +0200

    add StringHelper class

---
+++ src/staticMethods/StringHelper.java
@@ -0,0 +1,22 @@
+package staticMethods;
+
+public class StringHelper {
+
+
+    public static String multiply(String text) {
+        String result = "";
+        //write your code here
+        return result;
+    }
+
+    public static String multiply(String text, int count) {
+        String result = "";
+        //write your code here
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
