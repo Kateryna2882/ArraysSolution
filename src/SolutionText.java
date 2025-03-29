b1f4253c70b602bba5587f52d2fe6797cc57a72b
Kateryna2882 <govoryhina82@gmail.com>
2025-03-29 20:14:32 +0200

    add SolutionText class

---
+++ src/SolutionText.java
@@ -0,0 +1,39 @@
+public class SolutionText {
+
+    public static void main(String[] args) {
+        String numberStr = "147852369";
+        for (char symbol : numberStr.toCharArray()) {
+            System.out.print(digitToText(symbol));
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public static String digitToText(char digit) {
+        String result = "";
+        switch (digit) {
+            case '0':
+                result = "zero";
+            case '1':
+                result = "one";
+            case '2':
+                result = "two";
+            case '3':
+                result = "three";
+            case '4':
+                result = "four";
+            case '5':
+                result = "five";
+            case '6':
+                result = "six";
+            case '7':
+                result = "seven";
+            case '8':
+                result = "eight";
+            case '9':
+                result = "nine";
+        }
+        return result;
+    }
+
+}
