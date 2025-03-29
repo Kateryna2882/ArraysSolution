7a5a03b0607a532ad6bea6bd8227cdd5aeca3a46
Kateryna2882 <govoryhina82@gmail.com>
2025-03-29 19:54:55 +0200

    add LowerCase class

---
+++ src/LowerCase.java
@@ -0,0 +1,9 @@
+public class LowerCase {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        line = line.toLowerCase();
+        System.out.println(line);
+    }
+}
