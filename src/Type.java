c40ee24d2bc2ee04ae973ac6b36c94a64961cf9f
Kateryna2882 <govoryhina82@gmail.com>
2025-04-01 20:57:10 +0300

    add Type class

---
+++ src/Type.java
@@ -0,0 +1,11 @@
+public class Type {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = (byte) a + 46;
+        byte c = (byte) (a * b);
+        double f = (char) 1234.15;
+        long d = (short) (a + f / c + b);
+        System.out.println(d);
+    }
+}
