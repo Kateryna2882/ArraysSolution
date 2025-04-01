b28f4fb679c976483bb6859b8e8993d8980ca005
Kateryna2882 <govoryhina82@gmail.com>
2025-04-01 20:58:47 +0300

    add fields

--- src/Type.java
+++ src/Type.java
@@ -2,10 +2,10 @@ public class Type {
 
     public static void main(String[] args) {
         int a = 0;
-        int b = (byte) a + 46;
+        int b = a + 46;
         byte c = (byte) (a * b);
-        double f = (char) 1234.15;
-        long d = (short) (a + f / c + b);
+        double f = 1234.15;
+        long d = (long) (a + f / c + b);
         System.out.println(d);
     }
 }
