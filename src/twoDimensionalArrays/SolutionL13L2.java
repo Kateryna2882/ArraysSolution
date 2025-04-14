abf6d82622daa440a8bf2f330205271436a893e1
Kateryna2882 <govoryhina82@gmail.com>
2025-04-14 16:03:24 +0300

    add SolutionL13L2 class

---
+++ src/twoDimensionalArrays/SolutionL13L2.java
@@ -0,0 +1,14 @@
+package twoDimensionalArrays;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class SolutionL13L2 {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+
+        }
+    }
+
+
