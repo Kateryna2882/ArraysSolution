ff7fb03dc3f3dfb602eef17396b30955d7fee923
Kateryna2882 <govoryhina82@gmail.com>
2025-04-14 14:31:04 +0300

    add LargestNumber class

---
+++ src/twoDimensionalArrays/LargestNumber.java
@@ -0,0 +1,12 @@
+package twoDimensionalArrays;
+
+import java.util.ArrayList;
+
+public class LargestNumber {
+    private static ArrayList<Integer> integers = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+
+}
