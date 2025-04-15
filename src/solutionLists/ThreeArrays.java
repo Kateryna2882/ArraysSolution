d2795204ebfcbee7b89224d35291d3875eaa23fe
Kateryna2882 <govoryhina82@gmail.com>
2025-04-15 17:35:14 +0300

    add ThreeArrays class

---
+++ src/solutionLists/ThreeArrays.java
@@ -0,0 +1,20 @@
+package solutionLists;
+
+import java.util.ArrayList;
+
+public class ThreeArrays {
+
+    public static ArrayList<Integer> numbers = new ArrayList<>();
+    public static ArrayList<Integer> divBy3 = new ArrayList<>();
+    public static ArrayList<Integer> divBy2 = new ArrayList<>();
+    public static ArrayList<Integer> others = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+
+    public static void printList(ArrayList<Integer> list) {
+        //write your code here
+    }
+
+}
