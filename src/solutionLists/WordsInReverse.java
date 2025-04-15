d12546d1fc5ded7d4fcb4ebb2d408855c439d5b1
Kateryna2882 <govoryhina82@gmail.com>
2025-04-15 17:26:19 +0300

    add classes

---
+++ src/solutionLists/WordsInReverse.java
@@ -0,0 +1,25 @@
+package solutionLists;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class WordsInReverse {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter string " + (i + 1) + ": ");
+            list.add(reader.readLine());
+        }
+        list.remove(2);
+
+        for (int i = list.size() - 1; i >= 0; i--) {
+            String string = list.get(i);
+            System.out.println(string);
+        }
+    }
+}
+
+
