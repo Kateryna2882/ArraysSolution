42f32d1e676b0a9348cae84e0f43be6c75920106
Kateryna2882 <govoryhina82@gmail.com>
2025-04-17 18:21:26 +0300

    add RorL class

---
+++ src/solutionLists/RorL.java
@@ -0,0 +1,23 @@
+package solutionLists;
+
+import java.util.ArrayList;
+
+public class RorL {
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add("rose");
+        strings.add("love");
+        strings.add("lyra");
+        strings = fix(strings);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+
+    public static ArrayList<String> fix(ArrayList<String> strings) {
+        //write your code here
+        return null;
+    }
+
+}
