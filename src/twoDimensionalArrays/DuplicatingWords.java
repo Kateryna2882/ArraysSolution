13fdffd0833fba56d774ca5b4df71db221291863
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 15:04:02 +0300

    add DuplicatingWords class

---
+++ src/twoDimensionalArrays/DuplicatingWords.java
@@ -0,0 +1,29 @@
+package twoDimensionalArrays;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import static solutionarraylist.SolutionArrayList.list;
+
+public class DuplicatingWords {
+    public static void main(String[] args) throws Exception {
+        // Read strings from the console and declare an ArrayList here
+        ArrayList<String>strings = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+       String reader = reader.readLine();
+        for (String string : strings) {
+
+        }
+
+        ArrayList<String> result = doubleValues(list);
+
+        // Display result
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        //write your code here
+        return null;
+    }
+
+}
