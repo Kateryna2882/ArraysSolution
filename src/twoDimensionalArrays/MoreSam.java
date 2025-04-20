587b74e4f99ebdcf7ba08e4875da61a4e6520643
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 14:47:55 +0300

    add MoreSam class

---
+++ src/twoDimensionalArrays/MoreSam.java
@@ -0,0 +1,12 @@
+package twoDimensionalArrays;
+
+import java.util.ArrayList;
+
+public class MoreSam {
+    public static void main(String[] args) {
+        //write your code here
+        ArrayList<String> list = new ArrayList<>();
+
+    }
+
+}
