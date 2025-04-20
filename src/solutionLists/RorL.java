2802e7e0048f50053b7f950306fe8ca044738f0c
Kateryna2882 <govoryhina82@gmail.com>
2025-04-20 14:44:11 +0300

    add fields

--- src/solutionLists/RorL.java
+++ src/solutionLists/RorL.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class RorL {
     public static void main(String[] args) {
-        ArrayList<String> strings = new ArrayList<String>();
+        ArrayList<String> strings = new ArrayList<>();
         strings.add("rose");
         strings.add("love");
         strings.add("lyra");
@@ -17,7 +17,21 @@ public class RorL {
 
     public static ArrayList<String> fix(ArrayList<String> strings) {
         //write your code here
-        return null;
+        String r = "r";
+        String l = "l";
+        ArrayList<String> result = new ArrayList<>();
+        for (String string : strings) {
+            boolean isR = string.contains(r);
+            boolean isL = string.contains(l);
+
+            if (isR && !isL) {
+                continue;
+            }
+            if (!isR && isL) {
+                result.add(string);
+            }
+        }
+        return result;
     }
 
 }
