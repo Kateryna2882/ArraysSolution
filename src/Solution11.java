6b743a55df4d42606b9e74693b7a4aad6476a316
Kateryna2882 <govoryhina82@gmail.com>
2025-05-25 19:32:14 +0300

    add Solution11 class

---
+++ src/Solution11.java
@@ -0,0 +1,31 @@
+public class Solution11 {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndMaximum(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndMaximum(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<Integer, Integer>(null, null);
+        }
+
+        //write your code here
+
+        return new Pair<Integer, Integer>(0, 0);
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
