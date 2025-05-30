9d1142f24aec32fc60925746376a1d3fb84928f4
Kateryna2882 <govoryhina82@gmail.com>
2025-05-30 18:03:56 +0300

    add Solution1 class

---
+++ src/polimorfism/Solution1.java
@@ -0,0 +1,18 @@
+package polimorfism;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Cow cow = new Whale();
+
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+        public String getName() {
+            return "I'm a cow";
+        }
+    }
+
+    public static class Whale extends Cow {
+    }
+}
