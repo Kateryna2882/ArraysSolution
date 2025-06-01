3c59238557d9ec3a084dee8541ff3a888d66fc57
Kateryna2882 <govoryhina82@gmail.com>
2025-06-01 18:14:41 +0300

    add fields Solution3 class, add Solution4 class

--- src/overload/Solution3.java
+++ src/overload/Solution3.java
@@ -10,5 +10,23 @@ public class Solution3 {
     }
 
     //write your code here
+    public static void print(int a) {
+
+    }
+
+    public static void print(String b) {
+
+    }
+
+    public static void print(boolean c) {
+
+    }
+
+    public static void print (long l) {
+
+    }
+    public static void print (double d) {
+
+    }
 }
 
---
+++ src/overload/Solution4.java
@@ -0,0 +1,12 @@
+package overload;
+/*
+Three methods and a minimum
+
+*/
+public class Solution4 {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+}
