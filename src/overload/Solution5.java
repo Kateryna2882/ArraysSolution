22ffa8b7a5cbf1a4fca9336466670d1da396f8b9
Kateryna2882 <govoryhina82@gmail.com>
2025-06-01 18:45:35 +0300

    add fields

--- src/overload/Solution5.java
+++ src/overload/Solution5.java
@@ -10,5 +10,16 @@ public class Solution5 {
     }
 
     //write your code here
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static long max (long a, long b) {
+        return a > b ? a : b;
+    }
+
+    public static double max (double a, double b) {
+        return a > b ? a : b;
+    }
 }
 
