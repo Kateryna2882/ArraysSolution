24ec259b33eb7e8cb2b58de95851432bf9c29e4c
Kateryna2882 <govoryhina82@gmail.com>
2025-06-01 18:04:45 +0300

    add fields

--- src/overload/Solution2.java
+++ src/overload/Solution2.java
@@ -7,9 +7,18 @@ int and Integer
 
 public class Solution2 {
     public static void main(String[] args) {
-
+//        print(new Integer(1));
+        print(3);
+        print(5);
     }
 
     //write your code here
+    public static void print(int x) {
+
+    }
+
+    public static void print(Integer y) {
+
+    }
 }
 
