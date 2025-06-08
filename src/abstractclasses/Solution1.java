30c627ffc5199f99e607ab92204e281b002a9030
Kateryna2882 <govoryhina82@gmail.com>
2025-06-08 19:17:36 +0300

    add Solution1 class

---
+++ src/abstractclasses/Solution1.java
@@ -0,0 +1,18 @@
+package abstractclasses;
+
+/*
+Abstract Pet class
+
+*/
+public class Solution1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Pet {
+        public String getName() {
+            return "I'm a kitten";
+        }
+    }
+
+}
