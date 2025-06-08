ac16761e549e11cff85dd80be02c5e13ef53c63d
Kateryna2882 <govoryhina82@gmail.com>
2025-06-08 19:20:36 +0300

    add Solution2 class

---
+++ src/abstractclasses/Solution2.java
@@ -0,0 +1,19 @@
+package abstractclasses;
+
+/*
+"Fix the code", part 1
+
+*/
+public class Solution2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Pet {
+        public String getName() {
+            return "I'm a kitten";
+        }
+
+        public abstract Pet getChild();
+    }
+}
