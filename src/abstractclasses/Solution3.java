4b22950c17cac0912f407cf8e0f743e5e1b4d11b
Kateryna2882 <govoryhina82@gmail.com>
2025-06-15 13:12:15 +0300

    add Solution3 class

---
+++ src/abstractclasses/Solution3.java
@@ -0,0 +1,21 @@
+package abstractclasses;
+
+/*
+"Fix the code", part 2
+
+*/
+public class Solution3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static class Pet {
+        public String getName() {
+            return "I'm a kitten";
+        }
+
+        public Pet getChild();
+    }
+
+}
+
