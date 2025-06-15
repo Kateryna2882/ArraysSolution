a5b08ac34c1d1b4893d42a7759ac25b97aad4154
Kateryna2882 <govoryhina82@gmail.com>
2025-06-15 13:51:21 +0300

    add fields

--- src/abstractclasses/Solution5.java
+++ src/abstractclasses/Solution5.java
@@ -1,4 +1,42 @@
 package abstractclasses;
+/*
+Cats should not be abstract!
+
+*/
 
 public class Solution5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Pet {
+        public abstract String getName();
+
+        public abstract Pet getChild();
+    }
+
+    public static class Cat extends Pet {
+        @Override
+        public String getName(){
+           return "Cat";
+        }
+        @Override
+        public Pet getChild(){
+            return new Cat();
+        }
+
+    }
+
+    public static class Dog extends Pet {
+        @Override
+        public String getName(){
+            return "Dog";
+        }
+        @Override
+        public Pet getChild(){
+            return new Dog();
+        }
+    }
+
 }
+
