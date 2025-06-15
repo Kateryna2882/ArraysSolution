2810f65cab4020a2c87a13014ab646b1039b2e57
Kateryna2882 <govoryhina82@gmail.com>
2025-06-15 18:53:19 +0300

    add Solution5 class

---
+++ src/interfacepaket/Solution5.java
@@ -0,0 +1,15 @@
+package interfacepaket;
+
+/*
+Human class and CanRun and CanSwim interfaces
+
+*/
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+    }
+
+    // Add public interfaces and a public class here
+}
+
