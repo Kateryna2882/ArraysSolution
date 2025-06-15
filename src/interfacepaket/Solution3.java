4bcbd4c52fa1ebdd56f8320ae744945da3068e13
Kateryna2882 <govoryhina82@gmail.com>
2025-06-15 18:13:03 +0300

    add Solution3 class

---
+++ src/interfacepaket/Solution3.java
@@ -0,0 +1,35 @@
+package interfacepaket;
+/*
+Eat, fly, and move
+
+*/
+public class Solution3 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanMove {
+        public void move();
+    }
+
+    public interface CanEat {
+        public void eat();
+    }
+
+    public class Dog {
+    }
+
+    public class Duck {
+    }
+
+    public class Car {
+    }
+
+    public class Airplane {
+    }
+}
+
