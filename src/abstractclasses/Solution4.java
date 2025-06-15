3acb0871ceb8a3713ca38c7065147846406d0bca
Kateryna2882 <govoryhina82@gmail.com>
2025-06-15 13:29:08 +0300

    add Solution4 class

---
+++ src/abstractclasses/Solution4.java
@@ -0,0 +1,20 @@
+package abstractclasses;
+
+/*
+Cows are animals too
+
+*/
+
+public class Solution4 {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract class Animal {
+        public abstract String getName();
+    }
+
+    public static class Cow {
+    }
+
+}
