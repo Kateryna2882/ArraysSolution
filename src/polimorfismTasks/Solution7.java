71eca62dc39ceea9108475d7a31796e0ddeb7f3f
Kateryna2882 <govoryhina82@gmail.com>
2025-07-15 22:00:20 +0300

    add Solution7 class

---
+++ src/polimorfismTasks/Solution7.java
@@ -0,0 +1,37 @@
+package polimorfismTasks;
+
+/*
+Interfaces for the Human class
+
+*/
+public class Solution7 {
+    public static void main(String[] args) {
+        Human human = new Human();
+        System.out.println(human);
+    }
+
+    public static interface Employee {
+        public void workLazily();
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static interface Secretary {
+        public void workLazily();
+    }
+
+    public static interface Miner {
+        public void workVeryHard();
+    }
+
+    public static class Human {
+
+        public void workHard() {
+        }
+
+        public void workLazily() {
+        }
+    }
+}
