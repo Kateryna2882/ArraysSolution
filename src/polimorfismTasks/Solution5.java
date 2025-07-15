75178b53e739b5de0c61f9ee9385b5d8fe3b215a
Kateryna2882 <govoryhina82@gmail.com>
2025-07-15 21:24:27 +0300

    add Solution5 class

---
+++ src/polimorfismTasks/Solution5.java
@@ -0,0 +1,24 @@
+package polimorfismTasks;
+/*
+Climb, fly, and run
+
+*/
+
+public class Solution5 {
+    public static void main(String[] args) {
+
+    }
+
+    public class Cat {
+    }
+
+    public class Dog {
+    }
+
+    public class Tiger extends Cat {
+    }
+
+    public class Duck {
+    }
+
+}
