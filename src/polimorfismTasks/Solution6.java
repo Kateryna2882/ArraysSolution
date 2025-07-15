2014e599c8ec4efcb1496231098074f987e65bc7
Kateryna2882 <govoryhina82@gmail.com>
2025-07-15 21:47:28 +0300

    add Solution6 class

---
+++ src/polimorfismTasks/Solution6.java
@@ -0,0 +1,36 @@
+package polimorfismTasks;
+/*
+CanFly, CanRun, and CanSwim for the Duck, Penguin, and Toad classes
+
+*/
+
+public class Solution6 {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        public void fly();
+    }
+
+    public interface CanRun {
+        public void run();
+    }
+
+    public interface CanSwim {
+        public void swim();
+    }
+
+    public class Duck {
+
+    }
+
+    public class Penguin {
+
+    }
+
+    public class Toad {
+
+    }
+
+}
