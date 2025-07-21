1c3262a981c8f5cca17e8cd65e5b54257063ac9f
Kateryna2882 <govoryhina82@gmail.com>
2025-07-21 19:36:36 +0300

    add Solution11 class

---
+++ src/polimorfismTasks/Solution11.java
@@ -0,0 +1,23 @@
+package polimorfismTasks;
+/*
+Adding new functionality
+
+*/
+public class Solution11 {
+    public static void main(String[] args) {
+        Pegasus horse = new Pegasus();
+    }
+
+    public static interface CanFly {
+        public void fly();
+    }
+
+    public static class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus {
+    }
+}
