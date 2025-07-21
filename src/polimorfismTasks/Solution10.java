a97776cbdcdb9341274fda42f04b96515d3154d9
Kateryna2882 <govoryhina82@gmail.com>
2025-07-21 19:31:17 +0300

    add Solution10

---
+++ src/polimorfismTasks/Solution10.java
@@ -0,0 +1,33 @@
+package polimorfismTasks;
+/*
+Useless abstractions
+
+*/
+public class Solution10 {
+
+    public static void main(String[] args) {
+        Horse horse = new Pegasus();
+        horse.run();
+    }
+
+    public static interface CanFly {
+        public abstract void fly();
+    }
+
+    public static abstract class Horse {
+        public void run() {
+
+        }
+    }
+
+    public static class Pegasus extends Horse implements CanFly {
+        public abstract void fly() {
+
+        }
+    }
+
+    public static class SwimmingPegasus extends Pegasus {
+        public void swim();
+    }
+
+}
