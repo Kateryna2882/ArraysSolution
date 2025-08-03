24c73e8bbb6a9e651986597eeb3a4a6e2ec217f2
Kateryna2882 <govoryhina82@gmail.com>
2025-08-03 20:05:24 +0300

    add fields

--- src/interfaceTasks/Solution9.java
+++ src/interfaceTasks/Solution9.java
@@ -7,4 +7,13 @@ All that moves
 public class Solution9 {
     public static void main(String[] args) {
     }
+
+    interface CanMove {
+        Double speed();
+    }
+
+    interface CanFly extends CanMove {
+        Double speed(CanFly val);
+    }
 }
+
