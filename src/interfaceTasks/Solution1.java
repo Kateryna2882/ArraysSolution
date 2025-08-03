519ac2329210cf50bc4f02e2dd04d1df1191ac77
Kateryna2882 <govoryhina82@gmail.com>
2025-08-03 17:04:22 +0300

    add Solution1 class

---
+++ src/interfaceTasks/Solution1.java
@@ -0,0 +1,28 @@
+package interfaceTasks;
+/*
+Beer
+
+*/
+
+public class Solution1 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    public interface Drinker {
+        void askForMore(String message);
+
+        void sayThankYou();
+
+        boolean isReadyToGoHome();
+    }
+
+    public interface Alcoholic extends Drinker {
+        boolean READY_TO_GO_HOME = false;
+
+        void sleepOnTheFloor();
+    }
+
+    public static class BeerLover {
+
+    }
+}
