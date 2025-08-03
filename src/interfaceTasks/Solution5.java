46e71289c0685456aa87274f2646cb520e904266
Kateryna2882 <govoryhina82@gmail.com>
2025-08-03 18:26:19 +0300

    add fields

--- src/interfaceTasks/Solution5.java
+++ src/interfaceTasks/Solution5.java
@@ -8,27 +8,26 @@ public class Solution5 {
 
     public static void main(String[] args) {
 
-        System.out.println(new Dream().HOBBY.toString());
-        System.out.println(new Hobby().toString());
+        public static void main (String[]args){
+            System.out.println(Dream.HOBBY.toString());
+            System.out.println(new Hobby().toString());
+        }
 
-    }
+        interface Desire {
+        }
 
-    interface Desire {
-    }
+        interface Dream {
+            public static Hobby HOBBY = new Hobby();
+        }
 
-    interface Dream {
-        private static Hobby HOBBY = new Hobby();
-    }
+        static class Hobby implements Desire, Dream {
+            static int INDEX = 1;
 
-    class Hobby extends Desire implements Dream {
-        static int INDEX = 1;
-
-        @Override
-        public String toString() {
-            INDEX++;
-            return "" + INDEX;
+            @Override
+            public String toString() {
+                INDEX++;
+                return "" + INDEX;
+            }
         }
     }
-
 }
-
