20a2dc32bfaec3e1bd9aa049543afa0dd6211ddf
Kateryna2882 <govoryhina82@gmail.com>
2025-08-03 18:19:36 +0300

    add Solution5 class

---
+++ src/interfaceTasks/Solution5.java
@@ -0,0 +1,34 @@
+package interfaceTasks;
+/*
+Four mistakes
+
+*/
+
+public class Solution5 {
+
+    public static void main(String[] args) {
+
+        System.out.println(new Dream().HOBBY.toString());
+        System.out.println(new Hobby().toString());
+
+    }
+
+    interface Desire {
+    }
+
+    interface Dream {
+        private static Hobby HOBBY = new Hobby();
+    }
+
+    class Hobby extends Desire implements Dream {
+        static int INDEX = 1;
+
+        @Override
+        public String toString() {
+            INDEX++;
+            return "" + INDEX;
+        }
+    }
+
+}
+
