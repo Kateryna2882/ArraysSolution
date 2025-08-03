b612308abb5c2e9914b8df6bd4bf321fd44bce5f
Kateryna2882 <govoryhina82@gmail.com>
2025-08-03 19:35:54 +0300

    add Solution7 class

---
+++ src/interfaceTasks/Solution7.java
@@ -0,0 +1,18 @@
+package interfaceTasks;
+/*
+Parameterized interface
+
+*/
+
+public class Solution7 {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+
+    class StringObject //write your code here
+    {
+    }
+}
