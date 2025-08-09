83454bef5426183760054195f50be66938780eb6
Kateryna2882 <govoryhina82@gmail.com>
2025-08-09 19:28:47 +0300

    add Solution6 claas

---
+++ src/abstractclasses/Solution6.java
@@ -0,0 +1,21 @@
+package abstractclasses;
+/*
+Let's hire a translator
+
+*/
+
+public class Solution6 {
+    public static void main(String[] args) {
+        UkranianianTranslator russianTranslator = new UkranianianTranslator();
+        System.out.println(ukranianTranslator.translate());
+    }
+
+    public static abstract class Translator {
+        public abstract String getLanguage();
+
+        public String translate() {
+            return "I translate from " + getLanguage();
+        }
+    }
+
+}
