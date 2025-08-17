efcbc06d0b1a4d3e19aa018af24b3b6ea96ef6ba
Kateryna2882 <govoryhina82@gmail.com>
2025-08-17 19:26:48 +0300

    add Solution13 class

---
+++ src/abstractclasses/Solution13.java
@@ -0,0 +1,15 @@
+package abstractclasses;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+Reading a file
+
+*/
+public class Solution13 {
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
