e083ed6716f9f88fa8c9c3393c0e1d6a8c559b08
Kateryna2882 <govoryhina82@gmail.com>
2025-08-17 20:13:43 +0300

    add fields

--- src/abstractclasses/Solution14.java
+++ src/abstractclasses/Solution14.java
@@ -1,8 +1,6 @@
 package abstractclasses;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+
+import java.io.*;
 
 /*
 Writing to a file from the console
@@ -12,5 +10,21 @@ Writing to a file from the console
 public class Solution14 {
     public static void main(String[] args) {
         // write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String destinationFileName = reader.readLine();
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFileName));
+
+        StringBuilder builder = new StringBuilder();
+
+
+        String s = "";
+        while (!s.equals("exit")) {
+            s = reader.readLine();
+            builder.append(s).append("\n");
+        }
+
+        writer.write(builder.toString());
+        writer.close();
     }
 }
