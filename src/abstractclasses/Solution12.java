347e095195e1e171e9a887a8757189a0c1c38bf0
Kateryna2882 <govoryhina82@gmail.com>
2025-08-17 19:13:37 +0300

    add Solution12 class

---
+++ src/abstractclasses/Solution12.java
@@ -0,0 +1,25 @@
+package abstractclasses;
+/*
+Nice weather
+
+*/
+public class Solution12 {
+    public static void main(String[] args) {
+        System.out.println(new Today(WeatherType.CLOUDY));
+        System.out.println(new Today(WeatherType.FOGGY));
+        System.out.println(new Today(WeatherType.FREEZING));
+    }
+
+    static class Today {
+        private String type;
+
+        Today(String type) {
+            this.type = type;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Today it will be %s", this.getWeatherType());
+        }
+    }
+}
