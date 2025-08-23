f829ee4ada170d18f20151ff537ba2f42836b8a5
Kateryna2882 <govoryhina82@gmail.com>
2025-08-23 19:04:47 +0300

    add fields

--- src/abstractclasses/Solution15.java
+++ src/abstractclasses/Solution15.java
@@ -3,9 +3,10 @@ package abstractclasses;
 Neo
 
 */
-public class Solution15 { public static void main(String[] args) throws Exception {
-    System.out.println(Matrix.NEO);
-}
+public class Solution15 {
+    public static void main(String[] args) throws Exception {
+        System.out.println(Matrix.NEO);
+    }
 
     static class Matrix {
         public static DBObject NEO = new User().initializeIdAndName(1, "Neo");
@@ -15,9 +16,15 @@ public class Solution15 { public static void main(String[] args) throws Exceptio
         DBObject initializeIdAndName(long id, String name);
     }
 
-    static class User {
+    static class User implements DBObject {
         long id;
         String name;
+
+        public User initializeIdAndName(long id, String name) {
+            this.id = id;
+            this.name = name;
+            return this;
+        }
 
         @Override
         public String toString() {
