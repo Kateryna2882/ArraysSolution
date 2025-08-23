542c22c2c3d7e0dbc31c973a529954bd14f866f5
Kateryna2882 <govoryhina82@gmail.com>
2025-08-23 19:58:38 +0300

    add Solution21 class

---
+++ src/abstractclasses/Solution21.java
@@ -0,0 +1,11 @@
+package abstractclasses;
+/*
+Sorting even numbers from a file
+
+*/
+public class Solution21 {
+    public static void main(String[] args) {
+        //write your code here
+    }
+}
+
