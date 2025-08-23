333054bd44f487ee27343f52f494686804f7c828
Kateryna2882 <govoryhina82@gmail.com>
2025-08-23 19:18:30 +0300

    add Solution17 class

---
+++ src/abstractclasses/Solution17.java
@@ -0,0 +1,15 @@
+package abstractclasses;
+
+/*
+SimpleObject interface
+
+*/
+public class Solution17 {
+    public static void main(String[] args) throws Exception {
+        SimpleObject<String> stringObject = new StringObject();
+    }
+
+    interface SimpleObject<T> {
+        SimpleObject<T> getInstance();
+    }
+}
