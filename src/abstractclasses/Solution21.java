742735477d82f233c150023a3300ceb903dea621
Kateryna2882 <govoryhina82@gmail.com>
2025-08-23 20:13:20 +0300

    add fields

--- src/abstractclasses/Solution21.java
+++ src/abstractclasses/Solution21.java
@@ -1,11 +1,34 @@
 package abstractclasses;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
 /*
 Sorting even numbers from a file
 
 */
 public class Solution21 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //write your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String sourceFileName = reader.readLine();
+        FileInputStream fileInputStream = new FileInputStream(sourceFileName);
+        Scanner scanner = new Scanner(fileInputStream);
+        List<Integer>data = new ArrayList<>();
+        while (scanner.hasNext()){
+            int value = scanner.nextInt();
+            if (value % 2 == 0) data.add(value);
+        }
+        Collections.sort(data);
+        for (Integer value : data){
+            System.out.println(value);
+
+            scanner.close();
+            fileInputStream.close();
+        }
     }
 }
 
