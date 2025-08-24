f87a497838173912b52780b7bfea8dea179d225c
Kateryna2882 <govoryhina82@gmail.com>
2025-08-24 11:55:58 +0300

    add fields

--- src/abstractclasses/Solution22.java
+++ src/abstractclasses/Solution22.java
@@ -1,4 +1,5 @@
 package abstractclasses;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,4 +17,42 @@ public class Solution22 {
         plot.add(new Person("Granddaughter"));
         TurnipStory.tell(plot);
     }
+
+    public static class Person implements TurnipItem {
+        private String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        public void pull(Person second) {
+            System.out.println(name + " behind " + second.getName());
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+
+    public interface TurnipItem {
+        public String getName();
+    }
+
+    public class TurnipStory {
+        static void tell(List<Person> items) {
+            Person first;
+            Person second;
+            for (int i = items.size() - 1; i > 0; i--) {
+                first = items.get(i - 1);
+                second = items.get(i);
+                second.pull(first);
+            }
+        }
+    }
+
 }
