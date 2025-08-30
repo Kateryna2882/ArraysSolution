5f5d75b08ab94a31abffc7ac6adf5249c05417b4
Kateryna2882 <govoryhina82@gmail.com>
2025-08-30 19:51:38 +0300

    add fields

--- src/typeCasting/Solution3Food.java
+++ src/typeCasting/Solution3Food.java
@@ -17,19 +17,28 @@ public class Solution3Food {
 
     public static void foodMethods(Food food) {
         //write your code here
+        food.onEat();
+        food.onSelect();
     }
 
     public static void selectableMethods(Selectable selectable) {
         //write your code here
+        selectable.onSelect();
     }
 
-    interface Selectable {
+    interface Selectable  {
         void onSelect();
+
     }
 
-    static class Food {
+    static class Food implements Selectable{
         public void onEat() {
             System.out.println("The food was eaten");
+        }
+
+        @Override
+        public void onSelect() {
+            System.out.println("The food was selected");
         }
     }
 }
