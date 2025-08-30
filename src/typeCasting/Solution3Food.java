3855eabfda0546baffec0a2091f56feb0a7bcc17
Kateryna2882 <govoryhina82@gmail.com>
2025-08-30 19:41:57 +0300

    add Solution3Food class

---
+++ src/typeCasting/Solution3Food.java
@@ -0,0 +1,35 @@
+package typeCasting;
+
+/*
+Food
+
+*/
+
+public class Solution3Food {
+    public static void main(String[] args) {
+        Food food = new Food();
+        Selectable selectable = new Food();
+        Food newFood = (Food) selectable;
+
+        foodMethods(food);
+        selectableMethods(selectable);
+    }
+
+    public static void foodMethods(Food food) {
+        //write your code here
+    }
+
+    public static void selectableMethods(Selectable selectable) {
+        //write your code here
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    static class Food {
+        public void onEat() {
+            System.out.println("The food was eaten");
+        }
+    }
+}
