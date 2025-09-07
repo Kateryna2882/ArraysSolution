2d5adfeb64106da1eaffb3b1ed54f74f2c2391e5
Kateryna2882 <govoryhina82@gmail.com>
2025-09-07 11:50:07 +0300

    add SolutionChickenFactory class

---
+++ src/typeCasting/SolutionChickenFactory.java
@@ -0,0 +1,25 @@
+package typeCasting;
+
+/*
+Chicken factory
+
+*/
+
+public class SolutionChickenFactory {
+    public static void main(String[] args) {
+        Hen hen = HenFactory.getHen(Continent.AFRICA);
+        hen.getMonthlyEggCount();
+    }
+
+    static class HenFactory {
+
+        static Hen getHen(String continent) {
+            Hen hen = null;
+            //write your code here
+            return hen;
+        }
+    }
+
+
+}
+
