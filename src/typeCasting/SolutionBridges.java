b4ac5406c7dcd171f047018ce78ae1367b7ce6a5
Kateryna2882 <govoryhina82@gmail.com>
2025-09-07 18:01:58 +0300

    add SolutionBridges class

---
+++ src/typeCasting/SolutionBridges.java
@@ -0,0 +1,15 @@
+package typeCasting;
+
+/*
+Bridges
+
+*/
+public class SolutionBridges {
+    public static void main(String[] args) {
+        println(new WaterBridge());
+        println(new SuspensionBridge());
+    }
+
+    // Add println method here
+}
+
