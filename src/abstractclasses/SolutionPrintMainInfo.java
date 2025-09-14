c668626d2e712b167eaa324682d7bca11cd55b53
Kateryna2882 <govoryhina82@gmail.com>
2025-09-14 17:30:04 +0300

    add SolutionPrintMainInfo class

---
+++ src/abstractclasses/SolutionPrintMainInfo.java
@@ -0,0 +1,52 @@
+package abstractclasses;
+/*
+Implement the printMainInfo method
+
+*/
+public class SolutionPrintMainInfo {
+    public static void main(String[] args) {
+        Object obj = new Circle();
+        Movable movable = (Movable) obj;
+        Drawable drawable = new Rectangle();
+
+        printMainInfo(drawable);
+        printMainInfo(movable);
+    }
+
+    public static void printMainInfo(Object object) {
+        //write your code here
+
+    }
+
+    static interface Movable {
+
+        void move();
+    }
+
+    static class Circle implements Movable {
+
+        public void draw() {
+            System.out.println("Can be drawn");
+        }
+
+        public void move() {
+            System.out.println("Can be moved");
+        }
+
+    }
+
+    static interface Drawable {
+        void draw();
+    }
+
+    static class Rectangle implements Drawable {
+        public void draw() {
+            System.out.println("Can be drawn");
+        }
+
+        public void move() {
+            System.out.println("Can be moved");
+        }
+    }
+}
+
