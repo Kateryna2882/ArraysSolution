221861192de436b9557d0146d74d4c35b62ce457
Kateryna2882 <govoryhina82@gmail.com>
2025-09-21 20:14:11 +0300

    add Solution6 class

---
+++ src/bonusTaskLesson8/Solution6.java
@@ -0,0 +1,33 @@
+package bonusTaskLesson8;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Exception invasion
+
+*/
+
+public class Solution6 {
+    public static List<Exception> exceptions = new ArrayList<Exception>();
+
+    public static void main(String[] args) {
+        initExceptions();
+
+        for (Exception exception : exceptions) {
+            System.out.println(exception);
+        }
+    }
+
+    private static void initExceptions() {   // The first exception
+        try {
+            float i = 1 / 0;
+
+        } catch (Exception e) {
+            exceptions.add(e);
+        }
+
+        //write your code here
+
+    }
+}
+
