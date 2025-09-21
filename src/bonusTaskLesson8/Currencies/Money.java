aa99a79fa98d003c5c004379d7accbd74a8725e9
Kateryna2882 <govoryhina82@gmail.com>
2025-09-21 17:53:02 +0300

    add Solution4 class and Money class

---
+++ src/bonusTaskLesson8/Currencies/Money.java
@@ -0,0 +1,12 @@
+package bonusTaskLesson8.Currencies;
+
+public abstract class Money {
+    public Money(double amount) {
+    }
+
+    public abstract double getAmount();
+
+    public abstract String getCurrencyName();
+}
+
+
