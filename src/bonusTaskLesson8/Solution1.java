c41f8bcbd5301c881ea335d47c0410b28d70623b
Kateryna2882 <govoryhina82@gmail.com>
2025-09-21 17:08:00 +0300

    add fields

--- src/bonusTaskLesson8/Solution1.java
+++ src/bonusTaskLesson8/Solution1.java
@@ -11,53 +11,49 @@ public class Solution1 {
     public static void main(String[] args) throws Exception {
         // Read several keys (strings) from the console. Item 7
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        String key = null;
+        while ((key = reader.readLine()) != null) {
+            Movie movie = MovieFactory.getMovie(key);
+            if (movie == null) {
+                return;
+            }
+            System.out.println(movie.getClass().getSimpleName());
+        }
+    }
         /*
             8. Create a variable movie in the Movie class, and for each entered string (key):
             8.1. Get an object using MovieFactory.getMovie and assign it to the variable movie.
             8.2. Display the result of calling movie.getClass().getSimpleName().
         */
 
-    }
 
     static class MovieFactory {
-
         static Movie getMovie(String key) {
             Movie movie = null;
-
-            // Create a SoapOpera object for the key "soapOpera"
-            if ("soapOpera".equals(key)) {
-                movie = new SoapOpera();
+            switch (key) {
+                case "soapOpera":
+                    movie = new SoapOpera();
+                    break;
+                case "cartoon":
+                    movie = new Cartoon();
+                    break;
+                case "thriller":
+                    movie = new Thriller();
+                    break;
             }
-
-            if ("cartoon".equals(key)) {
-                return new Cartoon();
-            }
-
-            if ("thrller".equals(key)) {
-                return new Thriller();
-            }
-
-            //write your code here. Items 5, 6
-
             return movie;
         }
     }
 
     static abstract class Movie {
-        String movie;
     }
 
     static class SoapOpera extends Movie {
     }
 
-    // Write your classes here. Item 3
-    public static class Cartoon extends Movie {
-
+    static class Cartoon extends Movie {
     }
 
-    public static class Thriller extends Movie {
-
+    static class Thriller extends Movie {
     }
 }
-
